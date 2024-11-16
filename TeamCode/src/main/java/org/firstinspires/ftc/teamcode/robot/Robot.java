fb42e927bcee30c28ed4dc7234bae2373f1007fd
Oliver Rapp <oarapp3@gmail.com>
2024-11-16 11:02:39 -0500

    removed arm for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -1,15 +1,12 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drivetrain;
 
 public class Robot {
   public Drivetrain drivetrain;
-  public Arm arm;
 
   public Robot(HardwareMap hwMap) {
     drivetrain = new Drivetrain(hwMap);
-    arm = new Arm(hwMap);
   }
 }
