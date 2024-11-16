ec4a78bb9aafbb40d441d0bf9558c82269041847
Oliver Rapp <oarapp3@gmail.com>
2024-11-16 09:06:16 -0500

    updated gamepad drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCTeleOp.java
@@ -3,50 +3,79 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
-
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
-@TeleOp(name="Main TeleOp")
+@TeleOp(name = "Main TeleOp")
 public class FTCTeleOp extends OpMode {
-    private double deadZone = 0.0;
+  private Robot robot;
 
-    private Robot robot;
+  private double deadZone = 0.0;
+  private boolean drivetrainUseMaxSpeed = false;
 
-   @Override
-   public void init() {
-       robot = new Robot(this.hardwareMap);
+  //                           a      b      x      y
+  private boolean[] buttons = {false, false, false, false};
 
-       telemetry.addData(">", "Initialized");
-       telemetry.update();
-   }
+  @Override
+  public void init() {
+    robot = new Robot(this.hardwareMap);
 
-    @Override
-    public void loop() {
-       Gamepad g = gamepad2;
+    telemetry.addData(">", "Initialized");
+    telemetry.update();
+  }
 
-       double gpLeftStickY = g.left_stick_y;
-       double gpLeftStickX = g.left_stick_x;
-       double gpRightStickX = g.right_stick_x;
-       double gpDPadLeft = g.dpad_left;
-       double gpDPadRight = g.dpad_right;
+  @Override
+  public void loop() {
+    Gamepad g = gamepad2;
 
-       gpLeftStickY = Math.signum(gpLeftStickY) * (Math.abs(gpLeftStickY) * (1 - deadZone) + deadZone);
-       gpLeftStickX = Math.signum(gpLeftStickX) * (Math.abs(gpLeftStickX) * (1 - deadZone) + deadZone);
-       gpRightStickX = Math.signum(gpRightStickX) * (Math.abs(gpRightStickX) * (1 - deadZone) + deadZone);
-
-       if (gpDPadLeft) deadZone -= 0.01;
-       if (gpDPadRight) deadZone += 0.01;
-
-       double[] powers = robot.drivetrain.driveFluid(gpLeftStickY, gpLeftStickX, gpRightStickX);
-
-       //telemetry.addData("left y", gpLeftStickY);
-       //telemetry.addData("left x", gpLeftStickX);
-       //telemetry.addData("right x", gpRightStickX);
-       //telemetry.addData("front left power", powers[0]);
-       //telemetry.addData("front right power", powers[1]);
-       //telemetry.addData("back left power", powers[2]);
-       //telemetry.addData("back right power", powers[3]);
-       telemetry.addData("dead zone", deadZone);
-       telemetry.update();
+    boolean[] clicks = {false, false, false, false};
+    if (g.a && !buttons[0]) {
+      clicks[0] = true;
     }
+    if (g.b && !buttons[1]) {
+      clicks[1] = true;
+    }
+    if (g.x && !buttons[2]) {
+      clicks[2] = true;
+    }
+    if (g.y && !buttons[3]) {
+      clicks[3] = true;
+    }
+    buttons[0] = g.a;
+    buttons[1] = g.b;
+    buttons[2] = g.x;
+    buttons[3] = g.y;
+
+    double leftStickY = g.left_stick_y;
+    double leftStickX = g.left_stick_x;
+    double rightStickX = g.right_stick_x;
+    boolean dpadLeft = g.dpad_left;
+    boolean dpadRight = g.dpad_right;
+
+    if (dpadRight) deadZone += 0.01;
+    if (dpadLeft) deadZone -= 0.01;
+
+    if (clicks[0]) drivetrainUseMaxSpeed = !drivetrainUseMaxSpeed;
+    if (drivetrainUseMaxSpeed) {
+      double angle = Math.atan2(leftStickY, leftStickX);
+      leftStickX = Math.cos(angle);
+      leftStickY = Math.sin(angle);
+      if (rightStickX != 0) rightStickX = Math.signum(rightStickX) * 1;
+    } else {
+      leftStickY = Math.signum(leftStickY) * (Math.abs(leftStickY) * (1 - deadZone) + deadZone);
+      leftStickX = Math.signum(leftStickX) * (Math.abs(leftStickX) * (1 - deadZone) + deadZone);
+      rightStickX = Math.signum(rightStickX) * (Math.abs(rightStickX) * (1 - deadZone) + deadZone);
+    }
+
+    double[] powers = robot.drivetrain.driveFluid(leftStickY, leftStickX, rightStickX);
+
+    telemetry.addData("left y", leftStickY);
+    telemetry.addData("left x", leftStickX);
+    telemetry.addData("right x", rightStickX);
+    telemetry.addData("front left power", powers[0]);
+    telemetry.addData("front right power", powers[1]);
+    telemetry.addData("back left power", powers[2]);
+    telemetry.addData("back right power", powers[3]);
+    telemetry.addData("dead zone", deadZone);
+    telemetry.update();
+  }
 }
